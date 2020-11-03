2610a7f875b05a58a86a51a4ac34f760a0b0d567
TANG <tangziqi1226@gmail.com>
2020-11-03 11:05:41 +0100

    v9:38

---
+++ src/main/java/Model/Tp.java
@@ -0,0 +1,34 @@
+package Model;
+
+public class Tp {
+    protected Integer codeTp;
+    protected String nomTP;
+    public Tp() {
+    }
+
+
+
+
+
+
+
+
+
+
+    //get et set
+    public Integer getCodeTp() {
+        return codeTp;
+    }
+
+    public void setCodeTp(Integer codeTp) {
+        this.codeTp = codeTp;
+    }
+
+    public String getNomTP() {
+        return nomTP;
+    }
+
+    public void setNomTP(String nomTP) {
+        this.nomTP = nomTP;
+    }
+}
